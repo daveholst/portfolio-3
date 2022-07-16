b00fdd95d5d1172bccc1d8856575cc60a89b0fd2
Dave Holst <daveholst@gmail.com>
2022-07-16 13:13:59 +0800

    update static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
-import adapter from 'svelte-adapter-github';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
