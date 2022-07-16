dd3cf26c686efe5d1956379146aeb0436603d2ef
Dave Holst <daveholst@gmail.com>
2022-07-16 16:32:18 +0800

    fix component intellisense paths

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-
+import  path  from 'path'
 
 /** @type {import('vite').UserConfig} */
 const config = {
     plugins: [sveltekit()],
+    // Had to add the below to get the component import intellisense working
+        resolve: {
+          alias: {
+            src: path.resolve('./src')
+          }
+        }
+
 }
 
 export default config
