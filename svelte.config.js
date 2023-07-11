d7298c823bd38e92d792a7d811ce43dcd0692598
Dave Holst <daveholst@gmail.com>
2023-07-11 21:13:03 +0800

    bumped sveltekit, changed dir structure

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,22 @@
 // import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://github.com/sveltejs/svelte-preprocess
     // for more information about preprocessors
     preprocess: preprocess(),
-	kit: {
-		adapter: adapter({
-		  // default options are shown. On some platforms
-		  // these options are set automatically — see below
-		  pages: 'build',
-		  assets: 'build',
-		  fallback: null,
-		  precompress: false
-
-		}),
-
-		prerender: {
-		  // This can be false if you're using a fallback (i.e. SPA mode)
-		  default: true
-		}
-	  }
+    kit: {
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'build',
+            assets: 'build',
+            fallback: null,
+            precompress: false,
+        }),
+    },
 }
 
 export default config
