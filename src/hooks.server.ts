b46f91f8ab15f4e21ffa8c928da369a89d8b8121
Dave Holst <daveholst@gmail.com>
2023-08-01 22:10:39 +0800

    preloading again

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,6 @@
 export async function handle({ event, resolve }) {
     const response = await resolve(event, {
-        preload: ({ type, path }) =>
-            type === 'font' || path.includes('/fonts/'),
+        preload: ({ type }) => type === 'font',
     })
 
     return response
